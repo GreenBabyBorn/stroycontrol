ee964e4921359eb7a72204e83d6cec979ada5c76
GreenBabyBorngit config --global user.email greenbabyborn@gmail.comgit config --global user.name GreenBabyBorn <greenbabyborn@gmail.com>
2021-08-19 12:02:34 +0300

    Слайдер(swiper) + переход к секциям(якоря)

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ if (menuLinks.length > 0) {
 
 			this.classList.add('_active');
 
-
 			if (iconMenu.classList.contains('_active')) {
 				document.body.classList.remove('_lock');
 				iconMenu.classList.remove('_active');
@@ -48,3 +47,51 @@ if (menuLinks.length > 0) {
 	}
 
 }
+
+
+const footerLinks = document.querySelectorAll('.footer__item-link[data-goto]');
+
+if (footerLinks.length > 0) {
+	footerLinks.forEach(footerLinks => {
+
+		footerLinks.addEventListener("click", onFooterLinkClick);
+	});
+
+	function onFooterLinkClick(e) {
+		const menuLink = e.target;
+
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+
+	}
+
+}
+
+
+
+
+// Слайдер-Swiper
+new Swiper('.comments__slider-container', {
+
+	loop: true,
+	speed: 1000,
+
+	autoplay: {
+		delay: 2000,
+		disableOnInteraction: true,
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+
+});
+// Слайдер-Swiper
