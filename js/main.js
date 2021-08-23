9cfb4f1615aae2b74c2de4f2da28ec2c40486ed8
GreenBabyBorngit config --global user.email greenbabyborn@gmail.comgit config --global user.name GreenBabyBorn <greenbabyborn@gmail.com>
2021-08-23 14:55:34 +0300

    Последние правки

--- js/main.js
+++ js/main.js
@@ -99,42 +99,42 @@ new Swiper('.comments__slider-container', {
 //Анимация при скролле
 const animItems = document.querySelectorAll('._anim-items');
 
-if(animItems.length > 0){
+if (animItems.length > 0) {
 	window.addEventListener('scroll', animOnScroll);
-	function animOnScroll(params){
-		for(let index = 0; index < animItems.length; index++){
+	function animOnScroll(params) {
+		for (let index = 0; index < animItems.length; index++) {
 			const animItem = animItems[index];
-			const animItemHeight = animItem.offsetHeight ;
+			const animItemHeight = animItem.offsetHeight;
 			const animItemOffset = offset(animItem).top;
 			const animStart = 4;
-			
+
 			let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-			if(animItemHeight > window.innerHeight){
+			if (animItemHeight > window.innerHeight) {
 				animItemPoint = window.innerHeight - window.innerHeight / animStart;
 			}
 
-			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
 				animItem.classList.add('_active');
 			}
-			else{
-				if(!animItem.classList.contains('_anim-no-hide')){
+			else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
 					animItem.classList.remove('_active');
 				}
 			}
-			
+
 		}
 	}
-	function offset(el){
+	function offset(el) {
 		const rect = el.getBoundingClientRect(),
-		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		return{ top: rect.top + scrollTop, left: rect.left + scrollLeft}
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 	}
-	setTimeout(() =>{
+	setTimeout(() => {
 		animOnScroll();
 	}, 300)
-	
+
 }
 
 //Анимация при скролле
